a5f828a8928da31d07851cb542843a92a1fb5227
WillV1 <wmvanhook@gmail.com>
2020-04-22 22:19:50 -0400

    Committing updated changes to files per Wed updates

--- script.js
+++ script.js
@@ -1,145 +1,137 @@
 $(document).ready(function () {
-
-    var region;
-    var musicSelection;
-
     M.AutoInit();
     $(".dropdown-trigger").dropdown();
-
-
-
-    $("#firstHiddenContainer").hide();
-
-    $("#secondHiddenContainer").hide();
-
-    $("#thirdHiddenContainer").hide();
-
-    $("#fourthHiddenContainer").hide();
-
-    $('#fifthHiddenContainer').hide();
-
-    $('#sixthHiddenContainer').hide();
-
-    $("#lastContainer").hide();
-
-    $("#dropdown1").on("click", function (event) {
-        event.preventDefault();
-
-        $('h2').hide()
-        $('a').hide();
-        $('#firstHiddenContainer').show();
-
+    $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true
     });
-    $('#dropdown1').on("click", ".region", function () {
-        region = $(this).text();
-        console.log($(this))
-        console.log(region);
-
-    })
+    $("#carouselContainer").hide();
+    $("#closeBtn").hide();
+    $("#firstHiddenContainer").hide();
+    $("#thirdHiddenContainer").hide();
+    $("#fourthHiddenContainer").hide();
+    $('#fifthHiddenContainer').hide();
+    $("#lastContainer").hide();
+    $("#mealBtn").hide();
+    var region;
+    var musicSelection;
+    function displayRecipes(region) {
+        var apiId = "b8fa8ec0"
+        var apiKey = "2e99e135530eaed01cb9620b24c1f1c0"
+        var queryURL = "https://api.edamam.com/search?q=" + region + "&app_id=" + apiId + "&app_key=" + apiKey
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            //console.log(response);
+            $("#dropDownBtn").hide();
+            for (var i = 0; i < 3; i++) {
+                var regionRecipe = response.hits[i].recipe.url;
+                //console.log(regionRecipe);
+                var recipeName = response.hits[i].recipe.label;
+                console.log(recipeName);
+                var recipeImage = response.hits[i].recipe.image;
+                //console.log(recipeImage);
+                var col = $('<div id="recipeCard" class=“col s12 m4 l4”>').css({ "max-width": "32%", "min-height": "369px", "max-height": "370px", "overflow-y": "scroll" });
+                var card = $('<div class=“card blue-grey darken-1”>');
+                var content = $('<div class=“card-content white-text”>');
+                var title = $('<span class=card-title id="recipe">').text(recipeName);
+                var body = $('<div class=“card-action”>');
+                var img = $('<img>').attr('src', recipeImage).css({ 'max-width': '250px', 'max-height': '200px' });
+                var btn = $('<a>').attr('href', regionRecipe).text("Let me see this recipe!");
+                btn.attr("target", "_blank");
+                body.append(img, btn);
+                content.append(title, body);
+                card.append(content);
+                col.append(card);
+                //console.log(recipeImage);
+                $('#searchData').append(col);
+                $("#mealBtn").show();
+            };
+        });
+    };
     $("#recipe-button-one").on("click", function (eventTwo) {
         eventTwo.preventDefault();
-
-        $('#firstHiddenContainer').hide() &&
-
-            $('#secondHiddenContainer').show();
-
+        $('#firstHiddenContainer').hide()
     })
-
     $("#mealBtn").on("click", function (eventTwo) {
         eventTwo.preventDefault();
-
-        $('#secondHiddenContainer').hide() &&
-
-            $('#thirdHiddenContainer').show();
-
+        $("#searchData").hide();
+        $('#thirdHiddenContainer').show();
+        $("#mealBtn").hide();
     })
-
     $('#cocktailBtn').on('click', function (eventThree) {
         eventThree.preventDefault();
-
         $("#thirdHiddenContainer").hide() &&
             $('#fourthHiddenContainer').show();
-
     })
-
-
     $("#send").on("click", function (eventFive) {
         eventFive.preventDefault();
-
         $('#fourthHiddenContainer').hide() &&
             $('#fifthHiddenContainer').show();
-
-
-
         var liquor = $('.cocktail-textarea').val();
-
         var queryURLOne = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + liquor
-
-
         $.ajax({
             url: queryURLOne,
             method: "GET"
-
         })
             .then(function (response) {
-
                 var drinkChoices = [];
                 drinkChoices.push(response);
                 console.log(response)
-
-
                 var choice = drinkChoices[0].drinks[Math.floor(Math.random() * drinkChoices[0].drinks.length)];
-
-
-                var cocktailChoice = $('<p>')
-                cocktailChoice.text(choice.strDrink);
-                $('#drink-options').html(cocktailChoice);
-
+                var recipe = $("<a>");
+                recipe.attr("href", "https://www.thecocktaildb.com/drink.php?c=" + choice.idDrink + "-" + choice.strDrink.split(" ").join("-"));
+                recipe.attr("target", "_blank");
+                recipe.text(choice.strDrink);
+                recipe.addClass("link");
+                $('#drink-options').html(recipe);
+                console.log(recipe);
                 $('#cocktail-choice-two').on('click', function (eventEight) {
                     eventEight.preventDefault();
-
                     var button = $(this);
                     var buttonCount = (button.data("click_count") || 0) + 1;
                     button.data("click_count", buttonCount);
-
                     if (buttonCount == 1) {
                         var choice = drinkChoices[0].drinks[Math.floor(Math.random() * drinkChoices[0].drinks.length)];
-                        var cocktailChoice = $('<p>')
-                        cocktailChoice.text(choice.strDrink);
-                        $('#drink-options').html(cocktailChoice);
-
+                        var recipeTwo = $("<a>");
+                        recipeTwo.attr("href", "https://www.thecocktaildb.com/drink.php?c=" + choice.idDrink + "-" + choice.strDrink.split(" ").join("-"));
+                        recipeTwo.attr("target", "_blank");
+                        recipeTwo.text(choice.strDrink);
+                        recipeTwo.addClass("link");
+                        $('#drink-options').html(recipeTwo);
+                        console.log(recipeTwo);
                     } else {
+                        (buttonCount == 2)
+                        var choice = drinkChoices[0].drinks[Math.floor(Math.random() * drinkChoices[0].drinks.length)];
+                        var recipeThree = $("<a>");
+                        recipeThree.attr("href", "https://www.thecocktaildb.com/drink.php?c=" + choice.idDrink + "-" + choice.strDrink.split(" ").join("-"));
+                        recipeThree.attr("target", "_blank");
+                        recipeThree.text(choice.strDrink);
+                        recipeThree.addClass("link");
+                        $('#drink-options').html(recipeThree);
+                        console.log(recipeThree);
                         $('#cocktail-choice-two').hide()
                     }
-
-                })
-
-                $('#cocktail-choice').on('click', function (eventSeven) {
-                    eventSeven.preventDefault();
-
-                    $("#fifthHiddenContainer").hide() &&
-                        $('#sixthHiddenContainer').show();
-
-                    var recipe = $("<a>");
-                    recipe.attr("href", "https://www.thecocktaildb.com/drink.php?c=" + choice.idDrink + "-" + choice.strDrink.split(" ").join("-"));
-                    recipe.attr("target", "_blank");
-                    recipe.text(choice.strDrink);
-                    recipe.addClass("link");
-
-                    $("#drink-recipe").append(recipe);
                 });
+
             })
+
     })
+
     $('.playlist-menu').on('click', function (eventTen) {
         eventTen.preventDefault();
-
         if ($('#thirdHiddenContainer').show()) {
             $("#thirdHiddenContainer").hide() &&
                 $('#lastContainer').show();
-        } else {
-            $("#sixthHiddenContainer").hide()
-                && $('#lastContainer').show();
-        }
+        lastContainer();
+            } 
+        
+        function lastContainer () {
+            $("#fifthHiddenContainer").hide();
+                 $('#lastContainer').show();
+        };
+
 
         var array = [];
         $('span').each(function () {
@@ -147,10 +139,7 @@ $(document).ready(function () {
             regions = array.slice(0, 8);
         });
         console.log(regions);
-
-
         console.log(region);
-
         switch (region) {
             case 'American':
                 musicSelection = "christian+petermann";
@@ -189,19 +178,14 @@ $(document).ready(function () {
                 break;
         }
     })
-
-
     function music(musicSelection) {
         var queryURLTwo = 'https://api.jamendo.com/v3.0/albums/?client_id=cbf60db1&format=json&artist_name=' + musicSelection;
-
         $.ajax({
             url: queryURLTwo,
             method: "GET"
-
         })
             .then(function (response) {
                 console.log(response)
-
                 if (musicSelection == "christian+petermann") {
                     var link = $("<a>");
                     link.attr("href", response.results[0].shareurl);
@@ -262,16 +246,23 @@ $(document).ready(function () {
                     link.attr("target", "_blank");
                     link.text(response.results[0].name);
                     link.addClass("link");
-
                 }
-
                 $("#randomPlaylist").append(link);
-
             })
-
-
-
-
     }
-
+    $("#carouselBtn").on("click", function () {
+        $("#carouselContainer").show();
+        $("#carouselBtn").hide();
+        $("#closeBtn").show();
+    });
+    $("#closeBtn").on("click", function () {
+        $("#carouselContainer").hide();
+        $("#closeBtn").hide();
+    });
+    $('#dropdown1').on("click", ".region", function () {
+        region = $(this).text();
+        displayRecipes(region);
+        console.log($(this))
+        console.log(region);
+    })
 });
